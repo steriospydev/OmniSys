6fcfe6fe6ee48ac761aa03c61e50cd51b0d31ea6
sterios <steriospydev@gmail.com>
2024-02-15 19:34:02 +0200

    LoginView

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,10 +1,62 @@
 <template>
-<h1>Log in</h1>
+  <div>
+    <h1>Log in</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="error" style="color: red;">{{ error }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'LoginView',
-  components: { }
-}
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: ''
+    };
+  },
+  methods: {
+    login() {
+      const formData = {
+        username: this.username,
+        password: this.password
+      };
+
+      fetch('http://127.0.0.1:8000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else if (response.status === 401) {
+          throw new Error('Invalid username or password');
+        } else {
+          throw new Error('Error logging in');
+        }
+      })
+      .then(data => {
+        localStorage.setItem('token', data.token);
+        this.$router.push('/home');
+      })
+      .catch(error => {
+        this.error = error.message;
+      });
+    }
+  }
+};
 </script>
