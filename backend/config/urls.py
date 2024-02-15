1694caaf15f51d32907e67e82e1d2a542d92504e
sterios <steriospydev@gmail.com>
2024-02-15 19:51:07 +0200

    chane shema path to '/'

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.api.urls')),
-    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 
