de595a4911bd2984c2dc7f5fc92d13d4f4fa8170
sterios <steriospydev@gmail.com>
2024-02-17 20:37:15 +0200

    SetUp

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,5 +20,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('apps.account.api.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 ]
