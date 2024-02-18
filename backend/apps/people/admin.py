ee47657a856763f07faaa461b4ead1cd5c5bbea8
sterios <steriospydev@gmail.com>
2024-02-18 21:45:13 +0200

    Add supplier to admin panel

--- backend/apps/people/admin.py
+++ backend/apps/people/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Supplier
 # Register your models here.
+
+
+class SupplierAdmin(admin.ModelAdmin):
+    model = Supplier
+    list_display = ('company', 'email', 'is_active',
+                    'city', 'TIN_num', 'phone')
+
+
+admin.site.register(Supplier, SupplierAdmin)
+
