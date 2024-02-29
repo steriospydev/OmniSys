8f48be913e50d4463907d6b0a1f49e2bf34ce5ad
sterios <steriospydev@gmail.com>
2024-02-29 18:27:21 +0200

    add f

--- backend/apps/cashflow/apps.py
+++ backend/apps/cashflow/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TransactionConfig(AppConfig):
+class CashflowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transaction'
+    name = 'apps.cashflow'
