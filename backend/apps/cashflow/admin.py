f6213aaabc0d5fec6cb0c9af6f0afd945e999cb1
sterios <steriospydev@gmail.com>
2024-02-29 19:26:52 +0200

    fixed admin.py

--- backend/apps/cashflow/admin.py
+++ backend/apps/cashflow/admin.py
@@ -3,19 +3,19 @@ from django.contrib import admin
 from .models import (PayeeLabel, Payee, Payment,
                      Source, Income)
 
-@admin.site.register(Payee)
+@admin.register(Payee)
 class PayeeAdmin(admin.ModelAdmin):
     model = Payee
     list_display = ('name','label', 'active')
-    list_editable = ('active')
+    list_editable = ('active',)
 
-@admin.site.register(Payment)
+@admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     model = Payment
     list_display = ('payee','amount', 'payment_day','paid')
     list_editable = ('paid', 'payment_day')
     
-@admin.site.register(Income)
+@admin.register(Income)
 class IncomeAdmin(admin.ModelAdmin):
     model = Income
     list_display = ('source','amount','income_date')
